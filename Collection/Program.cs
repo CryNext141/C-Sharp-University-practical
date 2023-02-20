d54d1701283652e14e76700b8eb99918d8806668
CryNext141 <90177764+CryNext141@users.noreply.github.com>
2023-02-20 21:27:56 +0200

    Add files via upload

---
+++ Collection/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using SubClassForm;
+
+public class Task
+{
+    public static void Main(string[] args)
+    {
+        //Не бийте сильно за такі назви
+        Form retardOne = new Form("Kolinsky", 1994, "Gaming", true, 1500);
+        Form retardTwo = new Form("Alex", 2000, "Music", true, 870);
+        Form retardThree = new Form("Alex", 2003, "Aircraft", false, 110);
+        Form retardFour = new Form("Jonathan", 1994, "Weapon", true, 23);
+
+        var marriList = new List<bool>()
+        {
+            retardOne.freeMan,
+            retardTwo.freeMan,
+            retardThree.freeMan,
+            retardFour.freeMan
+        };
+
+        var duplicate = new List<string>()
+        {
+            retardOne.name,
+            retardTwo.name,
+            retardThree.name,
+            retardFour.name
+        };
+
+        Print();
+        avgSalary(retardOne.salary + retardTwo.salary + retardThree.salary + retardFour.salary);
+        marriCount(marriList);
+        duplicateNames(duplicate);
+
+        void Print()
+        {
+            retardOne.aboutMyself();
+            Console.WriteLine();
+            retardTwo.aboutMyself();
+            Console.WriteLine();
+            retardThree.aboutMyself();
+            Console.WriteLine();
+            retardFour.aboutMyself();
+            Console.WriteLine();
+        }
+
+        void avgSalary(int salary)  //Середня зарплата
+        {
+            Console.WriteLine();
+            double avg = salary / 4;
+            Console.WriteLine("Average salary: " + avg);
+        }
+
+        void marriCount(List<bool> married)  //Знаходжу кількість одружених 
+        {
+            int k = 0;
+
+            foreach (var item in married)
+            {
+                if (item)
+                {
+                    k++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            Console.WriteLine("How many married: " + k);
+        }
+
+        void duplicateNames(List<string> duplicate) //Знаю, знаю, знаю...Я без поняття як це зробити не використовуючи щось з колекції або без масива(що теж є колекцією)
+        {
+            Dictionary<string, int> freqMap = duplicate.GroupBy(x => x)
+                                            .Where(g => g.Count() > 1)
+                                            .ToDictionary(x => x.Key, x => x.Count());
+
+            Console.WriteLine("Duplicate names are: " + String.Join(",", freqMap));
+        }
+
+    }
+
+}
+
+ 
+
+
+
+    
---
+++ Collection/SubClassForm.cs
@@ -0,0 +1,39 @@
+﻿namespace SubClassForm
+{
+  
+    public class Form
+    {
+        public string name{ get; set; }
+        public int birthday { get; set; }
+        public string hobby { get; set; }
+        public bool freeMan { get; set; }
+        public int salary { get; set; }
+
+        public Form (string name, int birthday, string hobby, bool freeMan, int salary)
+        {
+            this.name = name;
+            this.birthday = birthday;
+            this.hobby = hobby;
+            this.freeMan = freeMan;
+            this.salary = salary;
+        }
+        public void aboutMyself()
+        {
+            string m;
+            if (this.freeMan)
+            {
+                m = "Married";
+            }
+            else
+            {
+                m = "Lonely";
+            }
+
+            Console.Write("Name: " + name + ", ");
+            Console.Write("Date of birth: " + birthday + ", ");
+            Console.Write("Hobby: " + hobby + ", " );
+            Console.Write("Family status: " + m + ", ");
+            Console.Write("Salary: " + salary+ " ");
+        }  
+    }
+}
