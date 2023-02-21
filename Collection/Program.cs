8f4dc425d864f1f6ef5c74b6f71be9105e56fba2
CryNext141 <90177764+CryNext141@users.noreply.github.com>
2023-02-21 08:57:17 +0200

    Update Program.cs

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using Microsoft.Data.SqlClient;
 using SubClassForm;
 
 public class Task
@@ -10,6 +11,8 @@ public class Task
         Form retardTwo = new Form("Alex", 2000, "Music", true, 870);
         Form retardThree = new Form("Alex", 2003, "Aircraft", false, 110);
         Form retardFour = new Form("Jonathan", 1994, "Weapon", true, 23);
+
+        string[] name = new string[] { retardOne.name, retardTwo.name, retardThree.name, retardFour.name };
 
         var marriList = new List<bool>()
         {
@@ -27,10 +30,12 @@ public class Task
             retardFour.name
         };
 
+
         Print();
         avgSalary(retardOne.salary + retardTwo.salary + retardThree.salary + retardFour.salary);
         marriCount(marriList);
-        duplicateNames(duplicate);
+        //duplicateNames(duplicate);
+        duplicatesName1(name);
 
         void Print()
         {
@@ -69,21 +74,30 @@ public class Task
             Console.WriteLine("How many married: " + k);
         }
 
-        void duplicateNames(List<string> duplicate) //Знаю, знаю, знаю...Я без поняття як це зробити не використовуючи щось з колекції або без масива(що теж є колекцією)
+        /*void duplicateNames(List<string> duplicate) //Знаю, знаю, знаю...Я без поняття як це зробити не використовуючи щось з колекції або без масива(що теж є колекцією)
         {
             Dictionary<string, int> freqMap = duplicate.GroupBy(x => x)
                                             .Where(g => g.Count() > 1)
                                             .ToDictionary(x => x.Key, x => x.Count());
 
             Console.WriteLine("Duplicate names are: " + String.Join(",", freqMap));
+        }*/
+
+        bool duplicatesName1(string[] arr)
+        {
+            {
+                var duplicates = arr
+                 .GroupBy(p => p)
+                 .Where(g => g.Count() > 1)
+                 .Select(g => g.Key);
+
+                Console.WriteLine("Duplicate names are: " + String.Join(",", duplicates));
+
+                return (duplicates.Count() > 0);
+
+            }
+
         }
 
-    }
-
+    } 
 }
-
- 
-
-
-
-    
