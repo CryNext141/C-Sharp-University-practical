c4b28f5d20dd22dd88754dc9963aaed6715b5e1c
CryNext141 <90177764+CryNext141@users.noreply.github.com>
2023-04-12 19:34:17 +0300

    Update GroupModule.cs

--- UniversityMange/GroupModule.cs
+++ UniversityMange/GroupModule.cs
@@ -1,34 +1,21 @@
-﻿using StudentModule;
-
+using StudentModule;
 
 namespace GroupModule
 {
     public class Group
     {
-        public int Id { get; set; }
+        public int ID { get; set; }
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public List<string> Subjects { get; set; }
 
         public Group(int id, string name)
         {
-            Id = id;
+            ID = id;
             Name = name;
             Students = new List<Student>();
             Subjects = new List<string>();
         }
 
-        //public void DisplayGroupInformation()
-        //{
-        //    Console.WriteLine("Group Information:");
-        //    Console.WriteLine($"Group ID: {Id}");
-        //    Console.WriteLine($"Group name: {Name}");
-        //    Console.WriteLine($"Number of students: {Students.Count}");
-        //    Console.WriteLine("Subjects:");
-        //    foreach (string subject in Subjects)
-        //    {
-        //        Console.WriteLine(subject);
-        //    }
-        //}
     }
 }
